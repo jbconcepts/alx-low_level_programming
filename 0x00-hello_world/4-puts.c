c854d8ae17ae8fa0880b51221a41a3e3508bfd25
Jibril Abubakar <abubakarjbgyang@gmail.com>
2022-09-12 21:54:02 -0700

    p

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,9 @@
 #include <stdio.h>
 
-
-
-// Program description: Program to put "Programming is like building a multilingual puzzle
-
 int main(void) {
 
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+			return (0);
 
 }
