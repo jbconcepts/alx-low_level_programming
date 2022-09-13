353734244ea55742914cb12dd2e151dcaae01e86
Jibril Abubakar <abubakarjbgyang@gmail.com>
2022-09-13 08:25:19 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+
+
+/**
+ *
+ * * main function 
+ *
+ * * Description: this is the main function
+ *
+ * * this program prints Programming is like building a multilingual puzzle using printf
+ *
+ * * return 0 
+ *
+ * */
+
+int main(void) {
+
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+
+			return (0);
+
+}
