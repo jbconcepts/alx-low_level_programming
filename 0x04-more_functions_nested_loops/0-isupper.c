b0e712ea1c0df7a72435062c59442b8ead6e150b
Jibril Abubakar <abubakarjbgyang@gmail.com>
2022-09-16 21:24:57 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *
+ *  * main - check the code.
+ *
+ *   *
+ *
+ *    * Return: Always 0.
+ *
+ *     */
+
+int main(void)
+{
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return(1);
+	}
+
+	return (0);
+}
